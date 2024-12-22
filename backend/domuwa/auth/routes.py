87a79a3bd0622b24fe167653ab15cae102d5a13b
tobasqo <szymon.j.jaskowski@gmail.com>
2024-12-22 23:05:30 +0100

    add scopes to login endpoint

--- backend/domuwa/auth/routes.py
+++ backend/domuwa/auth/routes.py
@@ -16,7 +16,7 @@ router = APIRouter(prefix="auth")
 @router.post("/login")
 async def login_for_access_token(
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
-    session: Session = Depends(get_db_session),
+    session: Annotated[Session, Depends(get_db_session)],
 ):
     user = authenticate_user(form_data.username, form_data.password, session)
     if not user:
@@ -27,7 +27,8 @@ async def login_for_access_token(
         )
     access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
-        {"sub": user.username}, expires_delta=access_token_expires
+        {"sub": user.username, "scopes": form_data.scopes},
+        expires_delta=access_token_expires,
     )
     return Token(access_token=access_token, token_type="bearer")
 
